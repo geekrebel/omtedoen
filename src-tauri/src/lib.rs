8f9762356cc4d19e90c5b5e5ba4b4cf11a563b3f
geekrebel <geekrebel@gmail.com>
2026-03-07 17:19:40 +1100

    fix(updater): add tauri-plugin-process and release v0.2.6 to fix automatic relaunch

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,6 +5,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_global_shortcut::Builder::new().build())
         .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_process::init())
         .setup(|app| {
             if cfg!(debug_assertions) {
                 app.handle().plugin(
